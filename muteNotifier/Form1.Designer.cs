b473bb1ab8debbaf85d91733b4ce5917b7fb2be4
warosusou <warosusou@outlook.com>
2021-03-09 02:15:23 +0900

    ポップアップで通知する簡易版

--- muteNotifier/Form1.Designer.cs
+++ muteNotifier/Form1.Designer.cs
@@ -30,12 +30,119 @@ namespace muteNotifier
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.volumeLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.endButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.updatingTimer = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(367, 33);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // volumeLabel
+            // 
+            this.volumeLabel.AutoSize = true;
+            this.volumeLabel.Location = new System.Drawing.Point(77, 197);
+            this.volumeLabel.Name = "volumeLabel";
+            this.volumeLabel.Size = new System.Drawing.Size(22, 25);
+            this.volumeLabel.TabIndex = 1;
+            this.volumeLabel.Text = "0";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 103);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(105, 43);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "開始";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // endButton
+            // 
+            this.endButton.Location = new System.Drawing.Point(274, 103);
+            this.endButton.Name = "endButton";
+            this.endButton.Size = new System.Drawing.Size(105, 43);
+            this.endButton.TabIndex = 3;
+            this.endButton.Text = "停止";
+            this.endButton.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "音量:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(196, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 31);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "通知を出すミュート時間";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(352, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "秒";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(391, 269);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.endButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.volumeLabel);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label volumeLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button endButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer updatingTimer;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- muteNotifier/Form1.cs
+++ muteNotifier/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NAudio.CoreAudioApi;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,10 +13,57 @@ namespace muteNotifier
 {
     public partial class Form1 : Form
     {
+        private double muteDuration;
+        private bool isMute;
+        
         public Form1()
         {
             InitializeComponent();
+            muteDuration = 0;
+            isMute = false;
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            var devices = enumerator.EnumerateAudioEndPoints(DataFlow.All,DeviceState.Active);
+            comboBox1.Items.AddRange(devices.ToArray());
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            endButton.Enabled = false;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null && textBox1.Text != "" && Double.TryParse(textBox1.Text,out var maxDuration))
+            {
+                startButton.Enabled = false;
+                endButton.Enabled = true;
+                updatingTimer = new Timer { Interval = 100, Enabled = true };
+                updatingTimer.Tick += (se, ev) =>
+                {
+                    updatingTimer.Enabled = false;
+                    if (comboBox1.SelectedItem != null)
+                    {
+                        var device = (MMDevice)comboBox1.SelectedItem;
+                        var volume = device.AudioMeterInformation.MasterPeakValue * 100;
+                        volumeLabel.Text = volume.ToString();
+                        if(volume <= 12.5 && !isMute)
+                        {
+                            isMute = true;
+                        }
+                        else if(volume <= 12.5 && isMute)
+                        {
+                            muteDuration += ((double)updatingTimer.Interval / 1000);//milliSeconds
+                            if(muteDuration >= maxDuration)
+                            {
+                                //通知
+                                MessageBox.Show("ミュート期間が設定値に達しました");
+                                muteDuration = 0;
+                            }
+                        }
+                        updatingTimer.Enabled = true;
+                    }
+                };
+            }
+        }
     }
 }
